56d78f371e1bab4b1c241587c7ecf2680b13303b
Sergey Bondar <sbondar@milosolutions.com>
2020-05-14 16:33:10 +0300

    fix migration

--- api/poc-api/apps/entities/migrations/0001_initial.py
+++ api/poc-api/apps/entities/migrations/0001_initial.py
@@ -13,7 +13,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('additional_data', '0002_auto_20200511_1516'),
         ('additional_data', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
